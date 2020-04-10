65fbbc3c6a55614dbb85a640783d69d0bd71bb60
Paul ologeh <paul.ologeh@outlook.com>
2020-04-10 21:55:26 +0100

    Indented main

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
  int main()
  {
-    simulation ACSE;
+	simulation ACSE;
 	ACSE.pizza.logging("\n*********** NEW SIMULATION ****************");
-    ACSE.printACSE();
-	
- 	std::cout << "Welcome to ACSE Pizza\n";
- 	std::cout << "This program simulates orders from our customers for 30 minutes\n";
- 	std::cout << "The program will start now. Cheers \n\n";	 
+	ACSE.printACSE();
+
+	std::cout << "Welcome to ACSE Pizza\n";
+	std::cout << "This program simulates orders from our customers for 30 minutes\n";
+	std::cout << "The program will start now. Cheers \n\n";	 
 	
     ACSE.simulate();
 
